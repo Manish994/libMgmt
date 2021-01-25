05555ff730ef354c366187b4c4a28c6982276213
Manish Pokhrel <paulsimon227@gmail.com>
2021-01-25 15:38:13 +0545

    Changes done in router.js

--- LibraryManagement/clientapp/src/router/routes.js
+++ LibraryManagement/clientapp/src/router/routes.js
@@ -8,6 +8,14 @@ const routes = [
     ]
   },
 
+  {
+    path: '/',
+    component: () => import('layouts/LoginRegister.vue'),
+    children: [
+      { path: 'auth/login', component: () => import('pages/login.vue') }
+    ]
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
