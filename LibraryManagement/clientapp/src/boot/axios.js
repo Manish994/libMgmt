a9bcf63457e71c53ce1d39b0909f8c8b9179f1a7
Manish Pokhrel <paulsimon227@gmail.com>
2021-02-18 21:46:01 +0545

    completed front-end and back-end validation

--- LibraryManagement/clientapp/src/boot/axios.js
+++ LibraryManagement/clientapp/src/boot/axios.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
+import { LocalStorage } from 'quasar'
 
 
 Vue.prototype.$axios = axios.create({
-    baseURL: 'https://localhost:44387/'
+    baseURL: 'https://localhost:44387/',
+    headers:{
+        Authorization:"Bearer" + " " + LocalStorage.getItem('user-token') 
+    }
 })
 
